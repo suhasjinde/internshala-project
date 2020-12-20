32c3feb2ba6352ccf0ee4a1f2b9d962d5ac315b7
Suhas Jinde <67955378+suhasjinde@users.noreply.github.com>
2020-12-20 21:59:36 +0530

    internshala smallbzr hiring task starting files

---
+++ functions.js
@@ -0,0 +1,37 @@
+function f1(){
+    var x = document.getElementById("menu-bar");
+        document.getElementById("menu-btn").style.display= "none";
+        x.style.display = "block";
+}
+function f2(){
+    document.getElementById("menu-bar").style.display="none";
+    document.getElementById("menu-btn").style.display="block";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
